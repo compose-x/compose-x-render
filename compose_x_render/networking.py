4a546d6950e712fc5badd6624893a0fca873d994
John Preston <john@ews-network.net>
2021-10-20 08:49:10 +0100

    Added tests covering and simplified code due to JSON Schema validation

--- compose_x_render/networking.py
+++ compose_x_render/networking.py
@@ -5,24 +5,6 @@
 import re
 
 
-def define_protocol(port_string):
-    """
-    Function to define the port protocol. Defaults to TCP if not specified otherwise
-
-    :param port_string: the port string to parse from the ports list in the compose file
-    :type port_string: str
-    :return: protocol, ie. udp or tcp
-    :rtype: str
-    """
-    protocols = ["tcp", "udp"]
-    protocol = "tcp"
-    if port_string.find("/"):
-        protocol_found = port_string.split("/")[-1].strip()
-        if protocol_found in protocols:
-            return protocol_found
-    return protocol
-
-
 def set_service_ports(ports):
     """Function to define common structure to ports
 
@@ -31,41 +13,34 @@ def set_service_ports(ports):
     """
     service_ports = []
     for port in ports:
-        if not isinstance(port, (str, dict, int)):
-            raise TypeError(
-                "ports must be of types", dict, "or", list, "got", type(port)
-            )
         if isinstance(port, str):
             ports_str_re = re.compile(
                 r"(?:(?P<published>\d{1,5})?(?::))?(?P<target>\d{1,5})(?:(?=/(?P<protocol>udp|tcp)))?"
             )
-            if not ports_str_re.match(port):
+            parts = ports_str_re.match(port)
+            print(parts)
+            if not parts:
                 raise ValueError(
                     f"Port {port} is not valid. Must match", ports_str_re.pattern
                 )
-            parts = ports_str_re.match(port)
-            service_ports.append(
-                {
+            port = {
                     "protocol": parts.group("protocol") or "tcp",
-                    "published": int(parts.group("published"))
-                    or int(parts.group("target")),
+                    "published": int(parts.group("published")) if isinstance(parts.group("published"), str) else
+                    int(parts.group("target")),
                     "target": int(parts.group("target")),
                     "mode": "awsvpc",
                 }
-            )
         elif isinstance(port, dict):
-            valid_keys = ["published", "target", "protocol", "mode"]
-            if not set(port).issubset(valid_keys):
-                raise KeyError("Valid keys are", valid_keys, "got", port.keys())
             port["mode"] = "awsvpc"
-            service_ports.append(port)
         elif isinstance(port, int):
-            service_ports.append(
-                {
+            port = {
                     "protocol": "tcp",
                     "published": port,
                     "target": port,
                     "mode": "awsvpc",
                 }
-            )
+        if port["published"] not in [s_port["published"] for s_port in service_ports]:
+            service_ports.append(port)
+        else:
+            print(f"Port {port['published']} is already defined", port, "Skipping")
     return service_ports
--- tests/test_compose_x_render.py
+++ tests/test_compose_x_render.py
@@ -4,21 +4,40 @@
 
 import pytest
 
-
-from compose_x_render import compose_x_render
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+from tempfile import TemporaryDirectory
+from compose_x_render.compose_x_render import ComposeDefinition
+from jsonschema.exceptions import ValidationError
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+from os import path
+
+HERE = path.abspath(path.dirname(__file__))
+
+
+def test_valid_input():
+    test = ComposeDefinition([f"{HERE}/valid_input.yaml"])
+    temp_dir = TemporaryDirectory()
+    test.write_output(output_file=f"{temp_dir.name}/test.yaml")
+    test.output_services_images(f"{temp_dir.name}/test.json")
+
+
+def test_valid_extension_input():
+    test = ComposeDefinition([f"{HERE}/valid_input.yaml", f"{HERE}/extension_input.yaml"])
+
+    temp_dir = TemporaryDirectory()
+    test.write_output(output_file=f"{temp_dir.name}/test.yaml", for_compose_x=True)
+
+
+def test_invalid_extension_input():
+    with pytest.raises(TypeError):
+        test = ComposeDefinition([f"{HERE}/valid_input.yaml", f"{HERE}/invalid_extension.yaml"])
+
+
+def test_fail_json_validation():
+    with pytest.raises(ValidationError):
+        test = ComposeDefinition([f"{HERE}/invalid_input.yaml"])
+
+
+def test_invalid_ports_format():
+    with pytest.raises(ValueError):
+        test = ComposeDefinition([f"{HERE}/invalid_port_format.yaml"])
